d54521debacb17f13d47cdf464bb10d6aa4bbc51
Lukas Tomasek <52459545+lukastomasek@users.noreply.github.com>
2020-06-02 13:05:30 -0400

    added content and style

--- js/app.js
+++ js/app.js
@@ -7,5 +7,31 @@ $todoContainer = document.querySelector(".todo-container");
 
 function addTodos(event) {
   event.preventDefault();
+
+  let todoDiv = document.createElement("div");
+  todoDiv.setAttribute("class", "todo");
+
+  let todoItem = document.createElement("li");
+  todoItem.setAttribute("class", "todo-li");
+  todoItem.innerText = "hi";
+
+  todoDiv.appendChild(todoItem);
+
+  let completeBtn = document.createElement("button");
+  completeBtn.setAttribute("class", "complete");
+  completeBtn.innerHTML = ` <i class="fas fa-plus-square"></i>`;
+
+  todoItem.appendChild(completeBtn);
+
+  let trashBtn = document.createElement("button");
+  trashBtn.setAttribute("class", "trash");
+  trashBtn.innerHTML = ` <i class="fas fa-plus-square"></i>`;
+
+  todoItem.appendChild(trashBtn);
+
+  $todoContainer.appendChild(todoDiv);
 }
+
+function removeToDo(event) {}
+
 $todoBtn.addEventListener("click", addTodos);
