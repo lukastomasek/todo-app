4339dbe1f8114ed66097044a8310bf7e36a7de2d
Lukas Tomasek <52459545+lukastomasek@users.noreply.github.com>
2020-06-03 12:00:01 -0400

    added more styling and js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 //TODO : create content in html, stylize the todo , add functionality , save to local files
 
-$todoInput = document.querySelector(".add-todo");
-$todoBtn = document.querySelector(".todo-btn");
-$filterBtn = document.querySelector("filter-todo");
-$todoContainer = document.querySelector(".todo-container");
+let $todoInput = document.querySelector(".add-todo");
+let $todoBtn = document.querySelector(".todo-btn");
+let $filterBtn = document.querySelector(".filter-todo");
+let $todoContainer = document.querySelector(".todo-list");
 
 function addTodos(event) {
   event.preventDefault();
@@ -12,26 +12,77 @@ function addTodos(event) {
   todoDiv.setAttribute("class", "todo");
 
   let todoItem = document.createElement("li");
-  todoItem.setAttribute("class", "todo-li");
-  todoItem.innerText = "hi";
+  todoItem.setAttribute("class", "todo-item");
+  todoItem.innerText = $todoInput.value;
 
   todoDiv.appendChild(todoItem);
 
   let completeBtn = document.createElement("button");
-  completeBtn.setAttribute("class", "complete");
-  completeBtn.innerHTML = ` <i class="fas fa-plus-square"></i>`;
+  completeBtn.setAttribute("class", "complete-btn");
+  completeBtn.innerHTML = ` <i class="fas fa-check"></i>`;
 
-  todoItem.appendChild(completeBtn);
+  completeBtn.addEventListener("click", function () {
+    todoDiv.classList.add("completed");
+  });
+
+  todoDiv.appendChild(completeBtn);
 
   let trashBtn = document.createElement("button");
-  trashBtn.setAttribute("class", "trash");
-  trashBtn.innerHTML = ` <i class="fas fa-plus-square"></i>`;
+  trashBtn.setAttribute("class", "trash-btn");
+  trashBtn.innerHTML = ` <i class="fas fa-trash"></i>`;
 
-  todoItem.appendChild(trashBtn);
+  trashBtn.addEventListener("click", removeToDo);
+
+  todoDiv.appendChild(trashBtn);
 
   $todoContainer.appendChild(todoDiv);
+  $todoInput.value = "";
 }
 
-function removeToDo(event) {}
+function removeToDo(event) {
+  let target = event.target;
+
+  if (target.classList[0] === "trash-btn") {
+    let todo = target.parentElement;
+    todo.remove();
+  }
+}
+
+function filterTodos(event) {
+  let todos = $todoContainer.childNodes;
+
+  todos.forEach(function (todo) {
+    switch (event.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+
+      default:
+        break;
+    }
+  });
+}
+
+function saveToLocal() {}
+
+function getTodos() {}
+
+function removeFromLocal() {}
+
+$filterBtn.addEventListener("click", filterTodos);
 
 $todoBtn.addEventListener("click", addTodos);
